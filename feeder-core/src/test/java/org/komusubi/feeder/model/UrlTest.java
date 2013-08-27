a63b2ca97d39c016736ad2639d19044911e79c60
jun ozeki <komusubi@gmail.com>
2013-08-28 05:55:17 +0900

    fix unit test #19

--- feeder-core/src/test/java/org/komusubi/feeder/model/UrlTest.java
+++ feeder-core/src/test/java/org/komusubi/feeder/model/UrlTest.java
@@ -20,14 +20,29 @@ package org.komusubi.feeder.model;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.junit.Ignore;
+import java.net.URL;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.komusubi.feeder.spi.UrlShortening;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 /**
  * @author jun.ozeki
  */
 public class UrlTest {
+
+    @Mock private UrlShortening urlShortening;
+    
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void delegationMethods() throws Exception {
@@ -41,14 +56,23 @@ public class UrlTest {
         assertThat(shorten.getProtocol(), equalTo("http"));
     }
 
-    // TODO issue #19
-    @Ignore
     @Test
-    public void shorten() {
-        Url shorten = new Url("https://githug.com/");
+    public void shorten() throws Exception {
+        // setup
+        String url = "https://github.com";
+        URL in = new URL(url);
+        URL out = new URL("https://bit.ly/dummy");
+        when(urlShortening.shorten(in)).thenReturn(out);
+
+        // exercise
+        Url shorten = new Url(url, urlShortening);
         Url once = shorten.shorten();
-        assertThat(shorten.toExternalForm(), equalTo(once.toExternalForm()));
         Url twice = shorten.shorten(); 
+        
+        // verify
+        assertThat(shorten.toExternalForm(), equalTo(once.toExternalForm()));
         assertThat(shorten.toExternalForm(), equalTo(twice.toExternalForm()));
+        // call Url#shorten twice but UrlShorten#shorten called only once.
+        verify(urlShortening, times(1)).shorten(in);
     }
 }
