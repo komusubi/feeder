555a56747df08106f68b1bf009734b4be18a80d4
jun ozeki <komusubi@gmail.com>
2013-10-10 07:40:14 +0900

    fix move file

--- feeder-sns-parent/feeder-sns-twitter/src/main/java/org/komusubi/feeder/sns/twitter/provider/TweetMessageProvider.java
+++ feeder-sns-parent/feeder-sns-twitter/src/main/java/org/komusubi/feeder/sns/twitter/provider/TweetMessageProvider.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.komusubi.feeder.sns.twitter.spi;
+package org.komusubi.feeder.sns.twitter.provider;
 
 import javax.inject.Provider;
 
