674d6d1bfef7cbdbf214ffe3c9a1d6d79c3d6afe
jun ozeki <komusubi@gmail.com>
2013-10-10 07:51:47 +0900

    fix change name

--- feeder-sns-parent/feeder-sns-twitter/src/main/java/org/komusubi/feeder/sns/twitter/provider/TweetMessagesProvider.java
+++ feeder-sns-parent/feeder-sns-twitter/src/main/java/org/komusubi/feeder/sns/twitter/provider/TweetMessagesProvider.java
@@ -20,20 +20,20 @@ package org.komusubi.feeder.sns.twitter.provider;
 
 import javax.inject.Provider;
 
-import org.komusubi.feeder.model.Message;
+import org.komusubi.feeder.model.Messages;
+import org.komusubi.feeder.sns.twitter.TweetMessage;
 
 /**
  * @author jun.ozeki
  */
-public class TweetMessageProvider implements Provider<Message> {
+public class TweetMessagesProvider implements Provider<Messages<TweetMessage>> {
 
     /**
      * @see javax.inject.Provider#get()
      */
     @Override
-    public Message get() {
-//        return new TweetMessage(new TweetScriptProvider("HHmm")); 
-        return null;
+    public Messages<TweetMessage> get() {
+        return new Messages<TweetMessage>();
     }
 
 }
