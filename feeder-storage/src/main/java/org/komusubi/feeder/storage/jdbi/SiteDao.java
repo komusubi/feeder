7a3bcda2670c1b0f15c83d2bc6171b8a277bda69
jun.ozeki <komusubi@gmail.com>
2014-07-15 07:58:48 +0900

    add files

---
+++ feeder-storage/src/main/java/org/komusubi/feeder/storage/jdbi/SiteDao.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.komusubi.feeder.storage.jdbi;
+
+import org.komusubi.feeder.model.WebSite;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+
+public interface SiteDao {
+    @SqlUpdate("create table sites ( id int auto_increment," 
+                                  + "name varchar(255),"
+                                  + "feed int,"
+                                  + "channel int,"
+                                  + "category int,"
+                                  + "url varchar(255),"
+                                  + "primary key (id),"
+                                  + "foreign key (feed) references feeds(id),"
+                                  + "foreign key (channel) references channels(id),"
+                                  + "foreign key (category) references categories(id)")
+    void createSiteTable();
+
+    @SqlQuery("select id, name, feed, channel, category, url from sites where id = :id")
+    WebSite findById(@Bind("id") int id);
+
+    void close();
+}
