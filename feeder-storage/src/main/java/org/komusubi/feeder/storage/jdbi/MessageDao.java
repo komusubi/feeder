9b360ea9d84465c791bbd1899a181b253a96fa83
jun.ozeki <komusubi@gmail.com>
2014-07-22 19:53:23 +0900

    add files

---
+++ feeder-storage/src/main/java/org/komusubi/feeder/storage/jdbi/MessageDao.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.komusubi.feeder.storage.jdbi;
+
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+
+/**
+ * @author jun.ozeki
+ */
+public interface MessageDao {
+
+    @SqlUpdate("create table messages (id int auto_increment,"
+                                    + "text varchar(1024),"
+                                    + "created timestamp,"
+                                    + "site_id int,"
+                                    + "primary key (id),"
+                                    + "foreign key (site_id) references sites(id)")
+    void createTable();
+
+    void close();
+}
+
---
+++ feeder-storage/src/main/java/org/komusubi/feeder/storage/mapper/ScriptMapper.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.komusubi.feeder.storage.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.komusubi.feeder.model.Message.Script;
+import org.komusubi.feeder.model.ScriptLine;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+public class ScriptMapper implements ResultSetMapper<Script> {
+
+    /**
+     * @see org.skife.jdbi.v2.tweak.ResultSetMapper#map(int, java.sql.ResultSet, org.skife.jdbi.v2.StatementContext)
+     */
+    @Override
+    public Script map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+        ScriptLine script = new ScriptLine(r.getString("text"));
+        return script;
+    }
+
+}
+
