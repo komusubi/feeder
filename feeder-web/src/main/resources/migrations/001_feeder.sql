6cc71802f7ab853221f513dd5b7481a94a46383a
jun.ozeki <komusubi@gmail.com>
2014-08-07 18:02:27 +0900

    fix create table sql

--- feeder-web/src/main/resources/migrations/001_feeder.sql
+++ feeder-web/src/main/resources/migrations/001_feeder.sql
@@ -2,50 +2,45 @@
 
 --changeset jun.ozeki:1
 create table feeds (
-    id int,
-    name varchar(64),
-    primary key(id)
+    id int primary key,
+    name varchar(64)
 );
 
 create table channels (
-    id int,
-    name varchar(64),
-    primary key(id)
+    id int primary key,
+    name varchar(64)
 );
 
 create table categories (
-    id int,
-    name varchar(64),
-    primary key(id)
+    id int primary key,
+    name varchar(64)
 );
 
 create table sites (
-    id int auto_increment,
+    id int auto_increment primary key,
     name varchar(255),
     feed int,
     channel int,
     category int,
     url varchar(255),
-    primary key (id),
     foreign key (feed) references feeds(id),
     foreign key (channel) references channels(id),
     foreign key (category) references categories(id)
 );
 
 create table messages (
-    id int auto_increment,
+    id int auto_increment primary key,
     text varchar(1024),
-    hash varchar(64),
+    hash varchar(64) unique,
     created timestamp,  
     site_id int,
-    primary key (id),
     foreign key (site_id) references sites(id)
 );
 
 create table tweets (
-    url varchar(255) unique,
+    url varchar(255) primary key,
     message_id int,
+    unique (url, message_id),
     foreign key (message_id) references messages(id)
 );
-create index tweet_idx on tweets(message_id);
 
