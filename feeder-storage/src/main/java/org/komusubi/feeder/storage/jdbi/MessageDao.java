3d74916af3ac1ce6371211116bdb58ae984761d0
jun.ozeki <komusubi@gmail.com>
2014-08-08 11:49:01 +0900

    fix reference

--- feeder-storage/src/main/java/org/komusubi/feeder/storage/jdbi/MessageDao.java
+++ feeder-storage/src/main/java/org/komusubi/feeder/storage/jdbi/MessageDao.java
@@ -21,7 +21,7 @@ package org.komusubi.feeder.storage.jdbi;
 import org.komusubi.feeder.model.Message;
 import org.komusubi.feeder.storage.jdbi.binder.MessageBinder;
 import org.komusubi.feeder.storage.jdbi.binder.MessageExistBinder;
-import org.komusubi.feeder.storage.jdbi.mapper.MessageMapper;
+import org.komusubi.feeder.storage.mapper.MessageMapper;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
